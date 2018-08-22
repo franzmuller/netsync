a1b127b23999774bac1ff3a9d8520a068d7651bd
franzmuller <42617099+franzmuller@users.noreply.github.com>
2018-08-22 18:33:59 +0000

    Create Deleted.cs

---
+++ Deleted.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace DataAccessLayer.Models
+{
+    public class Deleted
+    {
+        public long Id { get; set; }
+        public string TableName { get; set; }
+        public string KeyName { get; set; }
+        public string KeyValue { get; set; }
+
+        public DateTime Updated { get; set; }
+        public string UpdatedBy { get; set; }
+        public int SyncStatus { get; set; }
+    }
+}
